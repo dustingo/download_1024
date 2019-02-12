5a19eaf8f4c26786cc59814d7656740031a4eb74
dustingo <514838728@qq.com>
2019-02-12 15:34:15 +0800

    only crawl first page

---
+++ Download1024/settings.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for Download1024 project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'Download1024'
+
+SPIDER_MODULES = ['Download1024.spiders']
+NEWSPIDER_MODULE = 'Download1024.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'Download1024 (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'Download1024.middlewares.Download1024SpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+#    'Download1024.middlewares.Download1024DownloaderMiddleware': 543,
+    'Download1024.middlewares.UAMiddleware': None,
+}
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    'Download1024.pipelines.Download1024Pipeline': 300,
+#}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+#USER_AGENT = 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19'
+from fake_useragent import UserAgent
+ua = UserAgent()
+USER_AGENT = ua.random
+ROOT_URL = 'http://t66y.com/'
---
+++ Download1024/spiders/SmileSpider.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from bs4 import BeautifulSoup
+from Download1024.settings import  ROOT_URL
+import re
+from scrapy import Request
+import  os
+
+class SmilespiderSpider(scrapy.Spider):
+    root_url = ROOT_URL
+    name = 'SmileSpider'
+    allowed_domains = ['t66y.com']
+    start_urls = ['http://t66y.com/thread0806.php?fid=16&search=&page=1']
+    local_file_dir='D:/my1024pic/'
+    def parse(self, response):
+        content = response.body
+        soup_handler = BeautifulSoup(content,"html.parser")
+        template_article_list = soup_handler.find_all('a',attrs={"href":True,"id":True})
+        next_urls_list = soup_handler.find_all('a',string="下一頁")
+        last_urls_list  = soup_handler.find_all('a',attrs={"id":"last"})
+        next_urls = self.root_url + next_urls_list[0]['href']
+        last_urls = self.root_url + last_urls_list[0]['href']
+        article_url = []
+        match_reg='\w*\/\d*\/\d*\/\d*.html'
+        for item in template_article_list:
+            if re.match(match_reg,item['href']):
+                urls = self.root_url + item['href']
+                article_url.append(urls)
+        for url in article_url:
+            yield Request(url=url,callback=self.parse_img,meta={})
+
+
+    def parse_img(self, response):
+        img_list = []
+        content = response.body
+        soup_handler = BeautifulSoup(content,"html.parser")
+        temp_title_list = soup_handler.find_all('h4')
+        temp_img_url_list = soup_handler.find_all('input',attrs={"data-src":True})
+        title_name = re.sub('\?|:','',temp_title_list[0].text)
+        #print(title_name)
+        for img in temp_img_url_list:
+            if img['data-src']:
+                img_list.append(img['data-src'])
+        for img_url in img_list:
+            yield Request(url=img_url,callback=self.parse_download_img,meta={'title_name':title_name,'index':img_list.index(img_url)},dont_filter=True)
+
+
+    def parse_download_img(self, response):
+        content = response.body
+        title_name = response.meta['title_name']
+        index = response.meta['index']
+        img_format = response.url.split('.')[-1]
+        file_dir =self.local_file_dir + title_name + '/'
+        file_name = file_dir + str(index) + '.'+ img_format
+        if not os.path.exists(file_dir):
+            os.makedirs(file_dir)
+        with open(file_name,'xb') as f:
+            f.write(content)
+            print('{} {} {}'.format('IMG:',response.url,'[完成]'))
